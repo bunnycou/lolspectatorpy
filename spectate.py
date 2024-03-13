9ef7692777ed27e4db0d99d058ffa54e725b15e9
bunnycou <noahjcou@gmail.com>
2024-03-13 15:04:24 -0400

    updated api to use the new one with the tag and modified the displaying of the command

--- spectate.py
+++ spectate.py
@@ -3,8 +3,8 @@ import sys, requests, json
 config = json.loads(open("config.json", "r").read())
 API_KEY = config["key"]
 
-def PLAYER_API(name):
-    return f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={API_KEY}"
+def PLAYER_API(name, tag):
+    return f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={API_KEY}"
 def SPECTATOR_API(puuid):
     return f"https://na1.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/{puuid}?api_key={API_KEY}"
 
@@ -24,19 +24,25 @@ def main(args):
         puuid = False
 
     if not puuid: # if input was a name
-        player = player2puuid(player)
+        if "#" not in player:
+            tag = input("Input player tag: ")
+        else:
+            player, tag = player.split("#")
+        player = player2puuid(player, tag)
     
     gameID, encryptKey = puuid2game(player)
 
     print("Paste the following into command prompt: ")
-    print(f'cd /d "C:\Riot Games\League of Legends\Game" & "League of Legends.exe" "spectator spectator.na1.lol.pvp.net:8080 {encryptKey} {gameID} NA1" "-UseRads"')
+    commandstring1 = "cd /d \"C:\\Riot Games\\League of Legends\\Game\" & \"League of Legends.exe\" \"spectator spectator.na1.lol.pvp.net:8080"
+    commandstring2 = "NA1\" \"-UseRads\""
+    print(f"{commandstring1} {encryptKey} {gameID} {commandstring2}")
 
-def player2puuid(name):
-    response = requests.get(PLAYER_API(name))
+def player2puuid(name, tag):
+    response = requests.get(PLAYER_API(name, tag))
     if response.ok:
         return response.json()["puuid"]
     else:
-        print(f"Summoner Name Returned: {response.status_code}")
+        print(f"Username {name}#{tag} Returned: {response.status_code}")
         exit()
 
 def puuid2game(puuid):
